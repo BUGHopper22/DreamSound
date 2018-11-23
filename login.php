f3d5f76906d29ce66bff79e0d4be7bbbfcb2a42d
BUGHopper22 <alberto.bacco.1@gmail.com>
2018-11-23 17:14:39 +0100

    login e registrazione ok, rivedere qualche placeholder

--- login.php
+++ login.php
@@ -1,39 +1,43 @@
 <?php
+    // session_start();
     $contentActualPage='
     <div id="formContainer">
         <div class="loginContainer">
             <h2>Accedi</h2>
-            <form  class="formCenter" method="post" action="php/insertUser.php">
+            <form  class="formCenter" method="post" <!--<action="php/insertUser.php"--> >
                 <p>Username:</p>
-                <input type="text" name="userid">
+                <input type="text" name="loginUsername">
                 <p>Password:</p>
-                <input type="password" name="psw">
-                <input type="hidden" name="tasto" value="loginTasto">
+                <input type="password" name="loginPassword">
+                <!--<input type="hidden" name="tasto" value="loginTasto">-->
+                {loginError}
                 <input  class="formBtn" type="submit" value="Accedi">
             </form>
         </div>
         <div class="registerContainer">
             <h2>Registrati</h2>
-            <form class="formCenter" method="post" action="php/insertUser.php">
+            <form class="formCenter" method="post" <!--action="php/insertUser.php"--> >
                 <p>Inserisci un nome:</p>
-                <input type="text" name="name" placeholder="inserisci il nome">
+                <input type="text" name="name" placeholder="inserisci il nome" value="{nomeReload}">
                 <p>Inserisci un cognome:</p>
-                <input type="text" name="surname" placeholder="inserisci il cognome">
+                <input type="text" name="surname" placeholder="inserisci il cognome" value="{surnameReload}">
                 <p>Inserisci un username:</p>
-                <input type="text" name="userId" placeholder="inserisci il nome utente">
+                <input type="text" name="userId" placeholder="inserisci il nome utente" value="{userIdReload}">
                 <p>Inserisci una email:</p>
-                <input type="email" name="email" placeholder="inserisci email">
+                <input type="email" name="email" placeholder="inserisci email" value="{emailReload}">
                 <p>Scegli la tua password:</p>
                 <input type="password" name="psw1" placeholder="inserisci password">
                 <p>Conferma la password:</p>
-                <input type="password" name="psw2" placeholder="inserisci password">
-                <input type="hidden" name="tasto" value="registerTasto">
+                <input type="password" name="psw2" placeholder="inserisci password" >
+                <!--<input type="hidden" name="tasto" value="registerTasto">-->
                 <!-- <button type="button" type="submit" class="formBtn">Registrati</button> -->
+                {registerError}
                 <input  class="formBtn" type="submit" value="Registrati">
 
             </form>
         </div> 
     </div>';
+
     require_once('php/functions.php');	//è un include di function
     BuildPage("Login",$contentActualPage);	//funzione di buildpage dentro al file function
 ?>
--- php/functions.php
+++ php/functions.php
@@ -177,11 +177,128 @@ function insertProductList($titleTable,$category){
 
 
 //______________________________________________//
+//                LOGIN PAGE
+//______________________________________________//
+function insertVoidValueAndError($contentActualPage,$registerError){
+    $contentActualPage=str_replace('{nomeReload}','',$contentActualPage);
+    $contentActualPage=str_replace('{surnameReload}','',$contentActualPage);
+    $contentActualPage=str_replace('{userIdReload}','',$contentActualPage);
+    $contentActualPage=str_replace('{emailReload}','',$contentActualPage);
+    $contentActualPage=str_replace('{registerError}',$registerError,$contentActualPage);
+    $contentActualPage=str_replace('{loginError}','',$contentActualPage);
+    return $contentActualPage;
+}
+
+function createContetnLogin($contentActualPage){
+    require_once "./database/connessione.php";
+    
+    //entra nell' if sse è stato cliccato il tasto registrazione
+    if(isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['userId']) && isset($_POST['email']) && isset($_POST['psw1']) && isset($_POST['psw2'])){
+        $name=$_POST['name'];
+        $surname=$_POST['surname'];
+        $userId=$_POST['userId'];
+        $email=$_POST['email'];
+        $psw1=$_POST['psw1'];
+        $psw2=$_POST['psw2'];
+        
+        // query di controllo per checcare se userId è già presente nel DB
+        $resultUserId= mysqli_query($conn,"SELECT * FROM Utente WHERE Username='".$userId."'");
+        $contaResultUserId=mysqli_num_rows($resultUserId);
+        // query di controllo per checcare se email è già presente nel DB
+        $resultEmail= mysqli_query($conn,"SELECT * FROM Utente WHERE Email='".$email."'");
+        $contaResultEmail=mysqli_num_rows($resultEmail);
+        //controllo sulle lunghezze degli inserimenti
+        $checkName= strlen($name);
+        $checkSurname= strlen($surname);
+        $checkUserId= strlen($userId);
+        $checkPsw= strlen($psw1);
+        if($contaResultUserId==0 && $contaResultEmail==0 && $checkName>0 && $checkSurname>0 && $checkUserId>4 && $checkPsw>5 &&  $psw1==$psw2){//registrazione avvenuta con successo
+            //inserisce nel DB
+            $insert="INSERT INTO Utente(Username,Email,Password,Nome,Cognome) VALUES('".$userId."','".$email."','".md5($psw1)."','".$name."','".$surname."')";
+            $result = $conn->query($insert);
+            if($result){
+                $registerError='la query è andata a buon fine deve stampare utente registrato';
+                $contentActualPage=insertVoidValueAndError($contentActualPage,$registerError);
+                
+            }else{
+                echo 'queri andata malissimo magari erreori di server o boh';
+            }
+        }else{//errore di input per la registrazione
+            
+            $findError=0;//findError==0 => ancora nessun nessun errore trovato, findError==1 è stato trovato l'errore di input
+
+            //voglio vedere gli errori uno alla volta
+            if($checkName==NULL && $findError==0){
+                $registerError="inserisci un nome";
+                $findError=1;
+                
+            }
+            if($checkSurname==NULL && $findError==0){
+                $registerError="inserisci il cognome,";
+                $findError=1;
+            }
+            if($checkUserId<5 && $findError==0){
+                $registerError="inserisci un userId di minimo 6 caratteri";
+                $findError=1;
+            }
+            if($contaResultUserId>0 && $findError==0){
+                $registerError="username non disponibile,provane un altro";
+                $findError=1;
+                // substitution
+            }
+            if($contaResultEmail>0 && $findError==0){
+                $registerError="email già presente utilizzata,provane un' altra";
+                $findError=1;
+            }
+            if($checkPsw<6 && $findError==0){
+                $registerError="devi inserire una password,";
+                $findError=1;
+            }
+            if($psw1!=$psw2){
+                $registerError="hai inserito due password diverse";
+                $findError=1;
+            }
+            
+            //esempio di sostituzione value="{nomeReload}"
+            $contentActualPage=str_replace('{nomeReload}',$name,$contentActualPage);
+            $contentActualPage=str_replace('{surnameReload}',$surname,$contentActualPage);
+            $contentActualPage=str_replace('{userIdReload}',$userId,$contentActualPage);
+            $contentActualPage=str_replace('{emailReload}',$email,$contentActualPage);
+            $contentActualPage=str_replace('{registerError}',$registerError,$contentActualPage);
+        }
+    }else if(isset($_POST['loginUsername']) && isset($_POST['loginPassword'])){//entra qui solo se ho cliccato il tasto login
+        $userId=$_POST['loginUsername'];
+        $userPassword=$_POST['loginPassword'];
+        $resultUserId= mysqli_query($conn,"SELECT * FROM Utente WHERE Username='".$userId."' AND Password='".md5($userPassword)."' ");
+        $contaResultUserId=mysqli_num_rows($resultUserId);
+        if($contaResultUserId==1){
+            session_start();
+            echo("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP");
+            $_SESSION["sessionUserId"]=$userId;
+        }else{
+            
+        }
+        $contentActualPage=str_replace('{registerError}','',$contentActualPage);
+        $loginError="login avvenuta con successo";
+        $contentActualPage=str_replace('{loginError}',$loginError,$contentActualPage);
+
+
+    }else{//entra nell' esle sse non ho cliccato ne il tato registrazione ne login
+        $registerError='';
+        $contentActualPage=insertVoidValueAndError($contentActualPage,$registerError);
+    }
+    return $contentActualPage;
+    
+}
+
+
+//______________________________________________//
 //                 BUILD PAGE
 //______________________________________________//
 
 // ____SERVE PER COSTRUIRE LA PAGINA
 function BuildPage($title,$contentActualPage) {
+    echo("INIZIO PAGINA");
     // if(getcwd()==='C:\xampp\htdocs\tecnologieWeb'){
     $page=file_get_contents('./content/structure.html');//carica la struttura con head e body
     // }else{
@@ -203,11 +320,20 @@ function BuildPage($title,$contentActualPage) {
             $titleTable=$_REQUEST["ntab"];
         }
         $contentActualPage=insertProductList($titleTable,$title);
+    }else{
+        if($title=="Login"){
+                $contentActualPage=createContetnLogin($contentActualPage);
+        }else{
+
+        }
     }
     $page=str_replace('{content}',$contentActualPage,$page);
     //Aggiunta footer alla pagina
-    $footer=file_get_contents('content/footer.html');
-    $page=str_replace('{footer}',$footer,$page);
+    // $footer=file_get_contents('content/footer.html');
+    // $page=str_replace('{footer}',$footer,$page);
+    if(isset( $_SESSION["sessionUserId"])){
+        $page=$page."ciao ".$_SESSION["sessionUserId"];
+    }
     echo $page;
 }
 

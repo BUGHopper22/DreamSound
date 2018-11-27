ac03d0928fe5aea4e07a41dc76fe15df4e78a0bd
BUGHopper22 <alberto.bacco.1@gmail.com>
2018-11-27 17:56:42 +0100

    productDetail ok manca breadcrumb

--- productDetails.php
+++ productDetails.php
@@ -1,20 +1,34 @@
 <?php
+
+    // if(isset($_REQUEST["modello"])){
+    //     echo ("modello è definito");
+        $modello=$_REQUEST["modello"];
+        $marca=$_REQUEST["marca"];
+        $img=$_REQUEST["img"];
+        $descrizione=$_REQUEST["descrizione"];
+        $prezzo=$_REQUEST["prezzo"];
+        
+    // }
+    // else
+    //     echo("modello non è definito");
+
+
+
     $contentActualPage='
     <div id="productContainer">
-    <h1>Auricolari Apple EarPodss</h1>
+    <h1>'.$modello.'</h1>
         <div class="imgContainer">
-            <img class="productImg" src="./img/prodotti/cuffieInear/inear1.png" alt="prodotto1" >
+            <img class="productImg" src="./img/prodotti/'.$img.'" alt="'.$modello.'" >
         </div>
         <div class="product">
-            <p>Tipo connettore:Auricolare (jack 3.5 mm 4 poli)<br> Controllo del volume sul cavo: Sì<br> Fattore di forma
-                cuffie:In-ear<br> Impedenza:23 Ohm <br>Risposta frequenza:5 - 21000 Hz<br>
-                Sensibilità:109 dB/mW<br> Modalità uscita audio:Stereo<br>
-                Tecnologia di connessione:Cablato <br>Tipo prodotto:Cuffie con microfono<br> Peso:10.2
+            <p>
+                Marca: '.$marca.'<br>
+                Descrizione: '.$descrizione.'<br>
             </p>
         </div>
         <div class="buyContainer">
             <div class="productPrice">
-                <h3>Pezzo: 50$</h3>
+                <h3>Pezzo: '.$prezzo.'</h3>
             </div>
             <a class="compra" href="">
                 <p>Aggiungi al carrello</p>
@@ -22,5 +36,5 @@
         </div>
     </div>';
     require_once('php/functions.php');	//è un include di function
-    BuildPage("Dettagli prodotto",$contentActualPage);	//funzione di buildpage dentro al file function
+    BuildPage($modello,$contentActualPage);	//funzione di buildpage dentro al file function
 ?>

ed0098fa68dee44bf711b2221a631a826bef4aca
Giovanni Vidotto <giovanni_vidotto@hotmail.it>
2019-02-12 17:55:43 +0100

    Marge fix

--- amministratoreAggiungiProdotto.php
+++ amministratoreAggiungiProdotto.php
@@ -15,7 +15,6 @@ $contentActualPage='
 
 //_____________________________________AGGIUNGI PRODOTTO
     $contentActualPage.=
-<<<<<<< HEAD
     '<div class="adminContainer">
         <form class="formAdmin" method="post">
             <p>Categoria:</p>
@@ -39,33 +38,7 @@ $contentActualPage='
             <input class="formBtn distanceBtn" type="submit" name="submit" value="Aggiungi">
         </form>
     </div>
-</div>';
-=======
-    '<div class="modifyProductsList">
-    <h3>Aggiungi un prodotto</h3>
-    <form class="formAdmin" method="post">
-        <p>categoria:</p>
-        <select name="categoria" >';
-            $allCategory=allCategory($conn);
-            $contentActualPage=insertCategoryInSelect($contentActualPage,$allCategory);
-            $contentActualPage.=
-        '</select>
-        <p>modello:</p>
-        <input type="text" name="modello" placeholder="inserisci il modello" >
-        <p>marca:</p>
-        <input type="text" name="marca" placeholder="inserisci la marca" >
-        <p>prezzo:</p>
-        <input type="text" name="prezzo" placeholder="inserisci il prezzo" >
-        
-        <p>url immagine:</p>
-        <input type="text" name="urlImg" placeholder="inserisci url immagine" >
-        <p>Descrizione:</p>
-        <input type="text" name="descrizione" placeholder="inserisci una descrizione" >
-        
-        <input class="formBtn" type="submit" name="submit" value="Aggiungi">
-    </form></div>';
->>>>>>> ec03357c0f0013fcf9c12bf52be258ef952ea03f
-    
+</div>';    
     if(isset($_POST["submit"])){
         echo("hai cliccato su submit");
         $categoria=$_POST["categoria"];
